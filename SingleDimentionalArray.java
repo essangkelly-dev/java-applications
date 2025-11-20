b1134a6bd52e786802a4b5b4996a263528f68f89
essangkelly-dev <essangkelly@gmail.com>
2025-11-20 11:57:43 +0100

    Enums and single arrays

---
+++ SingleDimentionalArray.java
@@ -0,0 +1,10 @@
+public class SingleDimentionalArray{
+    public static void main(String[] args){
+	    String[] names = {"Ben","Daniel","Jack","Henry","Joy"};
+		int[] ages = {12,16,30,19,25};
+		
+		for(int i = 0; i < 5; i++){
+		System.out.printf("%s you are %d years old",names[i],ages[i]);
+		}
+	}
+}

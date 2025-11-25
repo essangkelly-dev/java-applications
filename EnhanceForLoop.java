09bbc113f58deaeb838209ddd6b9dbf527eb3a64
essangkelly-dev <essangkelly@gmail.com>
2025-11-25 12:08:12 +0100

    New

---
+++ EnhanceForLoop.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class EnhanceForLoop {
+    public static void main(String[] args) {
+        // create a single dimensional array of size 10
+        String[] names = new String[10];
+
+        // create a single dimensional array of size 10
+        int[] marks = new int[10];
+
+        // create the object of the scanner class
+        Scanner scan = new Scanner(System.in);
+
+        // get input from users using traditional for loop
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter Student name: ");
+            names[i] = scan.nextLine();
+
+            System.out.print("Enter Student mark: ");
+            marks[i] = scan.nextInt();
+            scan.nextLine(); // clear buffer
+
+            System.out.println("=========================");
+        }
+
+        // Display elements of names array
+        System.out.println("Elements of Names array are:");
+        for (String name : names) {
+            System.out.printf("%s%n", name);
+        }
+
+        // Display elements of marks array
+        System.out.println("Elements of Marks array are:");
+        for (int mark : marks) {
+            System.out.printf("%d%n", mark);
+        }
+    }
+}
